511a3af03e4e8a34740e63456f1c3f13e7f680c6
SamikshaD1 <dsamiksha258@gmail.com>
2024-01-20 17:17:57 +0530

    f

---
+++ src/components/Sign.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Sign = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = () => {
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Invalid email or password");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));
+
+        // Navigate to the profile page
+        navigate("/profile");
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
+  return (
+    <div className="flex items-center min-h-screen bg-white dark:bg-gray-900">
+      <div className="container mx-auto">
+        <div className="max-w-md mx-auto my-10 p-2 py-4 shadow-md shadow-slate-500 rounded">
+          <div className="text-start ml-7">
+            <p className="text-gray-500 dark:text-gray-400">Welcome back! 👋</p>
+            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
+              Sign in to your account
+            </h1>
+            <p className="text-red-500 mt-2">{errorMessage}</p>
+          </div>
+          <div className="m-7">
+            <form>
+              <div className="mb-6">
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                >
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email@company.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+              <div className="mb-6">
+                <div className="flex justify-between mb-2">
+                  <label
+                    htmlFor="password"
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    Password
+                  </label>
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                />
+              </div>
+
+              <div className="mb-6">
+                <button
+                  type="button"
+                  onClick={handleLogin}
+                  className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                >
+                  Sign in
+                </button>
+              </div>
+              <div className="mb-2 text-center">
+                <a
+                  href="#!"
+                  className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <p className="text-sm text-center text-gray-400">
+                Don't have an account yet?{" "}
+                <a
+                  href="#!"
+                  className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
+                >
+                  Sign up
+                </a>
+                .
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sign;
